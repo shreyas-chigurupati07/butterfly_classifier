f7104b306b4f2a44c39f8ed997dc74661c751cbb
Shreyas Chigurupati <84034817+shreyas-chigurupati07@users.noreply.github.com>
2025-01-27 09:44:32 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+__version__ = '0.1.0'
+
+
+REPO_NAME = 'butterfly_classifier'
+AUTHOR_USERNAME = 'shreyas-chigurupati07'
+SRC_REPO = 'cnnClassifier'
+AUTHOR_EMAIL = 'chigurupatishreyas@#gmail.com'
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USERNAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package to classify butterflies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USERNAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USERNAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+
+)
+
+
