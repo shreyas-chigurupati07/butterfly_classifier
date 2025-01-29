f558c560b21959a61e82c7920ed95cbd8b8fde79
Shreyas Chigurupati <84034817+shreyas-chigurupati07@users.noreply.github.com>
2025-01-29 09:56:30 -0800

    Prepare Base Model created

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 try:
@@ -10,4 +10,18 @@ try:
     logger.info(f'>>>>>>>>>> {STAGE_NAME} - Completed <<<<<<<<<<')
 except Exception as e:
     logger.error(f'Error in {STAGE_NAME} - {str(e)}')
+    raise e
+
+
+
+
+
+STAGE_NAME = 'Prepare Base Model Stage'
+try:
+    logger.info(f'>>>>>>>>>> {STAGE_NAME} - Started <<<<<<<<<<')
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f'>>>>>>>>>> {STAGE_NAME} - Completed <<<<<<<<<<')
+except Exception as e:
+    logger.exception(e)
     raise e
